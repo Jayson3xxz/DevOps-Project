e2886d5d0d5982499dc90a380dfe4440396f8d4e
Артур Сорокин <jayson3xz@MacBook-Pro-Artur.local>
2026-05-05 21:21:26 +0300

    Update: Working Responsing from Ollama

--- app/backend/backend_main.py
+++ app/backend/backend_main.py
@@ -10,25 +10,18 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.chat_history import InMemoryChatMessageHistory
 
-from working_classes import ChatRequest, ChatResponse, GuardDecision
+from working_classes import *
 
 
 OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://ollama:11434")
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3.2:3b")
-OLLAMA_TIME_OUT = int(os.getenv("OLLAMA_TIME_OUT", "20"))
+OLLAMA_TIME_OUT = int(os.getenv("OLLAMA_TIME_OUT", "120"))
 
 
 api = FastAPI(title="Ollama Backend")
 
 
 chat_histories: Dict[str, InMemoryChatMessageHistory] = {}
-
-
-def get_session_history(session_id: str) -> InMemoryChatMessageHistory:
-    if session_id not in chat_histories:
-        chat_histories[session_id] = InMemoryChatMessageHistory()
-
-    return chat_histories[session_id]
 
 
 llm = ChatOllama(
@@ -38,14 +31,9 @@ llm = ChatOllama(
     timeout=OLLAMA_TIME_OUT,
 )
 
-
-guardllm = ChatOllama(
-    model=OLLAMA_MODEL,
-    base_url=OLLAMA_BASE_URL,
-    temperature=0.0,
-    timeout=OLLAMA_TIME_OUT,
-    format="json",
-)
+prompt = ChatPromptTemplate(
+    [("system","You are AI-agent helper. Yout must anwer on Russian"), 
+     ("human", "{message}")])
 
 
 
@@ -61,20 +49,20 @@ async def health():
 
 @api.post("/chat", response_model=ChatResponse)
 async def post_message(request: ChatRequest):
-    
+    try:
+        chain = prompt | llm | StrOutputParser()
+        response = await chain.ainvoke(
+            {"message":request.message}
+        )
+
+        return {
+            "message": response,
+            "model":OLLAMA_MODEL
+        }
+    except:
+        return {
+            "message" : "Error:200",
+            "model":OLLAMA_MODEL
+        }
 
 
-@api.get("/chat/{session_id}/memory")
-async def get_memory(session_id: str):
-    history = get_session_history(session_id)
-
-    return {
-        "session_id": session_id,
-        "messages": [
-            {
-                "type": message.type,
-                "content": message.content,
-            }
-            for message in history.messages
-        ]
-    }
